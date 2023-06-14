9ed1e2c7b30f4f29a0a3d0b0b5d35241e932713a
br <brunno00amorim@gmail.com>
2023-06-13 23:51:02 -0300

    Adicionando props aos cards :robot_arm:

--- src/pages/Cards.js
+++ src/pages/Cards.js
@@ -2,7 +2,19 @@ import { Card, CardHeader, CardBody, CardFooter,Image,Stack,Heading,Text,Button 
 import robocactus from '../assests/robocactus.png'
 //transformar esse card em um componente que aceita props
 
-const Cards = () => {
+const Cards = ({title,text,img,left,buttonText}) => {
+  //Módulo I - Vamos falar sobre robótica?
+  // Neste módulo, daremos início aos nossos estudos de robótica. Abordaremos tópicos como a robótica industrial, presente em diversos setores da indústria, impulsionando a automação de processos e aumentando a eficiência produtiva. Também exploraremos a robótica educacional, desempenhando um papel importante no ensino e no desenvolvimento de habilidades. Além disso, mergulharemos na fascinante área da inteligência artificial, possibilitando aos robôs tomar decisões e interagir de forma inteligente com o ambiente. Por fim, teremos uma atividade relacionada ao módulo. Prepare-se para expandir seu conhecimento e explorar o mundo da robótica em suas diversas vertentes.
+
+  const toLeft = () => {
+    return( 
+  <Image
+    objectFit='cover'
+    maxW={{ base: '100%', sm: '200px' }}
+    src={img}
+    alt='Caffe Latte'
+    background='#338a9e'
+  /> )}
 
 return(
 <Card
@@ -10,29 +22,23 @@ return(
     overflow='hidden'
     variant='outline'
   >
-    <Image
-      objectFit='cover'
-      maxW={{ base: '100%', sm: '200px' }}
-      src={robocactus}
-      alt='Caffe Latte'
-      background='#338a9e'
-    />
-  
+    {left? toLeft(): null }
     <Stack>
       <CardBody>
-        <Heading size='md'>Módulo I - Vamos falar sobre robótica?</Heading>
+        <Heading size='md'>{title}</Heading>
   
-        <Text py='2'>
-        Neste módulo, daremos início aos nossos estudos de robótica. Abordaremos tópicos como a robótica industrial, presente em diversos setores da indústria, impulsionando a automação de processos e aumentando a eficiência produtiva. Também exploraremos a robótica educacional, desempenhando um papel importante no ensino e no desenvolvimento de habilidades. Além disso, mergulharemos na fascinante área da inteligência artificial, possibilitando aos robôs tomar decisões e interagir de forma inteligente com o ambiente. Por fim, teremos uma atividade relacionada ao módulo. Prepare-se para expandir seu conhecimento e explorar o mundo da robótica em suas diversas vertentes.
+        <Text py='2' textAlign="justify">
+        {text}
         </Text>
       </CardBody>
-  
-      <CardFooter>
+ 
+      <CardFooter >
         <Button variant='solid' colorScheme='blue'>
-          Simbora!
+        {buttonText}
         </Button>
       </CardFooter>
     </Stack>
+    {!left? toLeft(): null }
   </Card>)
 
 }
