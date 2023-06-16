99873cdf543635f508d889a426430d1d1fdc36c5
br <brunno00amorim@gmail.com>
2023-06-16 15:40:58 -0300

    :mechanical_arm: ajustes na homepage

--- src/pages/Cards.js
+++ src/pages/Cards.js
@@ -1,11 +1,7 @@
 import { Card, CardBody, CardFooter, Image, Stack, Heading, Text, Button } from '@chakra-ui/react'
-//transformar esse card em um componente que aceita props
 
 const Cards = ({ title, text, img, left, buttonText }) => {
-  //Módulo I - Vamos falar sobre robótica?
-  // Neste módulo, daremos início aos nossos estudos de robótica. Abordaremos tópicos como a robótica industrial, presente em diversos setores da indústria, impulsionando a automação de processos e aumentando a eficiência produtiva. Também exploraremos a robótica educacional, desempenhando um papel importante no ensino e no desenvolvimento de habilidades. Além disso, mergulharemos na fascinante área da inteligência artificial, possibilitando aos robôs tomar decisões e interagir de forma inteligente com o ambiente. Por fim, teremos uma atividade relacionada ao módulo. Prepare-se para expandir seu conhecimento e explorar o mundo da robótica em suas diversas vertentes.
-
-  const toLeft = () => {
+   const toLeft = () => {
     return (
       <Image
         objectFit='cover'
@@ -34,7 +30,7 @@ const Cards = ({ title, text, img, left, buttonText }) => {
 
         <CardFooter >
           {left ? (
-            <Button variant='solid' colorScheme='blue'>
+            <Button variant='solid' colorScheme='blue' >
               {buttonText}
             </Button>
           ) : (
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,62 +1,62 @@
 import React from 'react';
 import {
-  Box,
-  Button,
-  ChakraProvider,
-  Container,
-  Flex,
-  FormControl,
-  FormLabel,
-  Heading,
-  Input,
-  Stack,
-  Text,
-  useColorModeValue,
+    Box,
+    Button,
+    ChakraProvider,
+    Container,
+    Flex,
+    FormControl,
+    FormLabel,
+    Heading,
+    Input,
+    Stack,
+    Text,
+    useColorModeValue,
 } from '@chakra-ui/react';
 import { useNavigate } from 'react-router';
 import { goToHome } from '../routes/cordinator';
 import Header from '../components/Header'
 
 const LoginPage = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  return (
+    return (
 
-      <Container maxW="md" mt={8} justifyContent="center" alignItems="center"
-     >
-    <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-            ROBOCACTUS APP
-        </Flex>
-        </Box>
-        
-        <Box bg="white" p={8} borderRadius="md" boxShadow="md" >
-          <Heading mb={4}>Login</Heading>
-          <form>
-            <Stack spacing={3}>
-              <FormControl id="email">
-                <FormLabel>Email</FormLabel>
-                <Input type="email" />
-              </FormControl>
+        <Container maxW="md" mt={8} justifyContent="center" alignItems="center"
+        >
+            <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+                <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+                    ROBOCACTUS APP
+                </Flex>
+            </Box>
 
-              <FormControl id="password">
-                <FormLabel>Password</FormLabel>
-                <Input type="password" />
-              </FormControl>
+            <Box bg="white" p={8} borderRadius="md" boxShadow="md" >
+                <Heading mb={4}>Login</Heading>
+                <form>
+                    <Stack spacing={3}>
+                        <FormControl id="email">
+                            <FormLabel>Email</FormLabel>
+                            <Input type="email" />
+                        </FormControl>
 
-              <Button colorScheme="blue" type="submit" onClick={() => goToHome(navigate)}>
-                Login
-              </Button>
-            </Stack>
-          </form>
+                        <FormControl id="password">
+                            <FormLabel>Password</FormLabel>
+                            <Input type="password" />
+                        </FormControl>
 
-          <Text mt={4} textAlign="center">
-            Não tem uma conta? <Text as="span" color="blue.500">cadastre-se</Text>
-          </Text>
-        </Box>
-      </Container>
-   
-  );
+                        <Button colorScheme="blue" type="submit" onClick={() => goToHome(navigate)}>
+                            Login
+                        </Button>
+                    </Stack>
+                </form>
+
+                <Text mt={4} textAlign="center">
+                    Não tem uma conta? <Text as="span" color="blue.500">cadastre-se</Text>
+                </Text>
+            </Box>
+        </Container>
+
+    );
 };
 
 export default LoginPage;
